1d51e72e915db0a974f8a259fe62346626e5291a
RonaldKirk443 <ronaldkirk101pc@gmail.com>
2023-05-01 16:50:05 -0400

    Created springboot app

---
+++ src/main/java/com/uio443/vglbackend/VglBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uio443.vglbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VglBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VglBackendApplication.class, args);
+    }
+
+}
